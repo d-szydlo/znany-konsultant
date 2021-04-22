3edeadb943ec653a9c756954b245e96e72242b40
Paweł Data <123pawel3@wp.pl>
2021-04-22 17:27:37 +0200

    Add temporary register layout

--- app/src/main/java/com/example/znanykonultant/RegistrationActivity.kt
+++ app/src/main/java/com/example/znanykonultant/RegistrationActivity.kt
@@ -2,10 +2,29 @@ package com.example.znanykonultant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.FragmentManager
+import com.example.znanykonultant.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun showUserFragment(view: View) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.registrationFragment, UserRegisterFragment())
+        transaction.commit()
+    }
+
+    fun showConsultantFragment(view: View) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.registrationFragment, KonsultantRegisterFragment())
+        transaction.commit()
     }
 }
