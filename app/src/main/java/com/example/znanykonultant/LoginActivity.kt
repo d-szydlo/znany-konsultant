643cf7e34bf39aa75443a5dfb65fe6917f605681
kaszycaO <o_kaszyca@interia.pl>
2021-05-06 16:28:07 +0200

    Db basic config

--- app/src/main/java/com/example/znanykonultant/LoginActivity.kt
+++ app/src/main/java/com/example/znanykonultant/LoginActivity.kt
@@ -3,9 +3,15 @@ package com.example.znanykonultant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity(){
 
@@ -20,6 +26,27 @@ class LoginActivity : AppCompatActivity(){
         loginField = findViewById(R.id.loginField)
         passwordField = findViewById(R.id.passwordField)
         loginButton = findViewById(R.id.loginButton)
+
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+        myRef.setValue("Hejcia")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.value
+                loginField.setText("Value is $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+
+
+
     }
 
     fun onLoginClick(v : View){
@@ -27,7 +54,7 @@ class LoginActivity : AppCompatActivity(){
         var password = passwordField.text
         val myintent = Intent(this, MainPageActivity::class.java)
         startActivityForResult(myintent, 1)
-        //var intent = Intent()
+
     }
 
     fun onRegisterClick(v : View){
--- app/src/main/java/com/example/znanykonultant/MainActivity.kt
+++ app/src/main/java/com/example/znanykonultant/MainActivity.kt
@@ -3,12 +3,13 @@ package com.example.znanykonultant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
 
