f5334b41a947964785c463f9200abfe93a5d09a1
Paweł Data <123pawel3@wp.pl>
2021-05-12 12:34:48 +0200

    Add navigation bar to consultant page

--- app/src/main/java/com/example/znanykonultant/consultant/ConsultantMainPageActivity.kt
+++ app/src/main/java/com/example/znanykonultant/consultant/ConsultantMainPageActivity.kt
@@ -2,29 +2,39 @@ package com.example.znanykonultant.consultant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.util.Log
+import androidx.fragment.app.Fragment
 import com.example.znanykonultant.R
+import com.example.znanykonultant.consultant.appointments.ConsultantAppointmentsFragment
+import com.example.znanykonultant.consultant.messages.ConsultantMessagesFragment
+import com.example.znanykonultant.consultant.profile.ConsultantProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ConsultantMainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_consultant_main_page)
 
-        createRecyclerViews()
+        setFragment(ConsultantProfileFragment())
+        prepareNavigation()
     }
 
-    private fun createRecyclerViews() {
-        val conversationsRecycler = findViewById<RecyclerView>(R.id.conversationsRecycler)
-        conversationsRecycler.layoutManager = LinearLayoutManager(this)
-        conversationsRecycler.adapter = ConsultantConversationListAdapter(genTempData())
-
-        val appointmentsRecycler = findViewById<RecyclerView>(R.id.appointmentsRecycler)
-        appointmentsRecycler.layoutManager = LinearLayoutManager(this)
-        appointmentsRecycler.adapter = ConsultantAppointmentsListAdapter(genTempData())
+    private fun setFragment(newFragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, newFragment)
+        transaction.commit()
     }
 
-    private fun genTempData(): List<String> {
-        return listOf("abc", "def", "xyz")
+    private fun prepareNavigation() {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.consultant_nav_profile -> setFragment(ConsultantProfileFragment())
+                R.id.consultant_nav_messages -> setFragment(ConsultantMessagesFragment())
+                R.id.consultant_nav_appointments -> setFragment(ConsultantAppointmentsFragment())
+                else -> Log.e("famousConsultant", "consultant navigation, Unknown fragment id = ${it.itemId}")
+            }
+            true
+        }
     }
 }
--- app/src/main/java/com/example/znanykonultant/consultant/appointments/ConsultantAppointmentsListAdapter.kt
+++ app/src/main/java/com/example/znanykonultant/consultant/appointments/ConsultantAppointmentsListAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.znanykonultant.consultant
+package com.example.znanykonultant.consultant.appointments
 
 import android.view.LayoutInflater
 import android.view.View
