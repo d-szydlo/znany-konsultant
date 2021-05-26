11c0330b23b48bf637a77eb16cc73c07ccbb6dbf
mateuszkochanek <mateuszkochanek01@gmail.com>
2021-05-26 13:56:33 +0200

    Adds buttons to user consultant view

--- app/src/main/java/com/example/znanykonultant/user/consultant/profile/UserConsultantProfileActivity.kt
+++ app/src/main/java/com/example/znanykonultant/user/consultant/profile/UserConsultantProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.znanykonultant.user.consultant.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.znanykonultant.R
 
 class UserConsultantProfileActivity : AppCompatActivity() {
@@ -9,15 +10,13 @@ class UserConsultantProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_consultant_profile)
         var uid = this.intent.getStringExtra("consultant_uid")
-        var newFragment: UserConsultantProfileFragment = UserConsultantProfileFragment(uid)
+        var newFragment: UserConsultantProfileFragment = UserConsultantProfileFragment(uid) // TODO przechowywanie uid przy obrocie, lub zablokowanie obrotu
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragmentContainerProfil, newFragment)
         transaction.commit()
     }
 
-    private fun setFragment(newFragment: UserConsultantProfileFragment) {
-
-    }
-
-
+    fun makeAnAppointment(view: View) {}
+    fun addOpinion(view: View) {}
+    fun startChatting(view: View) {}
 }
