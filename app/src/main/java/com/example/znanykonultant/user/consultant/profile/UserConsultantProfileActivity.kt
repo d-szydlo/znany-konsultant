d0a38b98211ae6c59164c481ef6745523ddb741f
Paweł Data <123pawel3@wp.pl>
2021-06-03 12:16:02 +0200

    Add possibility to enter new chat

--- app/src/main/java/com/example/znanykonultant/user/consultant/profile/UserConsultantProfileActivity.kt
+++ app/src/main/java/com/example/znanykonultant/user/consultant/profile/UserConsultantProfileActivity.kt
@@ -5,7 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.znanykonultant.R
+import com.example.znanykonultant.chat.ChatsFragment
+import com.example.znanykonultant.chat.SingleChatActivity
+import com.example.znanykonultant.entity.Consultant
 import com.example.znanykonultant.user.consultant.profile.opinion.OpinionActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class UserConsultantProfileActivity : AppCompatActivity() {
     lateinit var consultantUid : String
@@ -30,5 +37,26 @@ class UserConsultantProfileActivity : AppCompatActivity() {
         startActivity(myIntent)
     }
 
-    fun startChatting(view: View) {}
+    fun startChatting(view: View) {
+        val intent = Intent(this, SingleChatActivity::class.java)
+        intent.putExtra(ChatsFragment.NAME_KEY, getConsultantName(consultantUid))
+        intent.putExtra(ChatsFragment.UID_KEY, consultantUid)
+        startActivity(intent)
+    }
+
+    private fun getConsultantName(uid: String) : String {
+        var name = ""
+        val consultantRef = FirebaseDatabase.getInstance().getReference("consultants/${uid}")
+
+        consultantRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val consultant = snapshot.getValue(Consultant::class.java) ?: return
+                name = "${consultant.name} ${consultant.surname}"
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+        return name
+    }
 }
