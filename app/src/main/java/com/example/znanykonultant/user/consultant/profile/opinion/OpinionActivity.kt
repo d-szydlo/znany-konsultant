e32f1285bb2f992ab54c96b41390054ca1c22913
mateuszkochanek <mateuszkochanek01@gmail.com>
2021-06-05 14:39:48 +0200

    Implements opinion editing

--- app/src/main/java/com/example/znanykonultant/user/consultant/profile/opinion/OpinionActivity.kt
+++ app/src/main/java/com/example/znanykonultant/user/consultant/profile/opinion/OpinionActivity.kt
@@ -2,14 +2,18 @@ package com.example.znanykonultant.user.consultant.profile.opinion
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.znanykonultant.chat.LatestMessageRow
+import com.example.znanykonultant.consultant.ConsultantMainPageActivity
 import com.example.znanykonultant.databinding.ActivityOpinionBinding
 import com.example.znanykonultant.entity.Messages
 import com.example.znanykonultant.entity.Opinion
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.lang.Math.round
 import kotlin.math.roundToInt
 
@@ -17,12 +21,22 @@ import kotlin.math.roundToInt
 class OpinionActivity : AppCompatActivity() {
     lateinit var binding: ActivityOpinionBinding
     lateinit var consultantUid : String
+    private val userId = FirebaseAuth.getInstance().uid
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOpinionBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         consultantUid = this.intent.getStringExtra("consultant_uid").toString()
+
+        Firebase.database.getReference().child("opinions").child(userId.toString()).child(consultantUid).get().addOnSuccessListener{
+            if(it.value != null){
+                var opinion = it.getValue(Opinion::class.java)
+                binding.title.text = Editable.Factory.getInstance().newEditable(opinion?.title)
+                binding.rating.rating = opinion?.rating!!
+                binding.descriptiveOpinion.text = Editable.Factory.getInstance().newEditable(opinion.description)
+            }
+        }
     }
 
     fun saveOpinion(view: View) {
@@ -31,7 +45,6 @@ class OpinionActivity : AppCompatActivity() {
         val title = binding.title.text.toString()
         val description = binding.descriptiveOpinion.text.toString()
         val consultantId = consultantUid
-        val userId = FirebaseAuth.getInstance().uid
         val reference = FirebaseDatabase.getInstance().getReference(
             "opinions/$userId/$consultantUid"
         )
@@ -47,6 +60,11 @@ class OpinionActivity : AppCompatActivity() {
         resultIntent.putExtra("consultant_uid", consultantUid)
         setResult(RESULT_OK, resultIntent)
         finish()
+    }
+
+
+    fun deleteOpinion(view: View) {
+
     }
 
     fun updateConsultantAverage(){
@@ -74,41 +92,5 @@ class OpinionActivity : AppCompatActivity() {
                 // TODO
             }
         })
-        /*consultantReference.addChildEventListener(object: ChildEventListener {
-            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
-                var avg  = 0f
-                var count  = 0
-                var opinion = dataSnapshot.getValue(Opinion::class.java) ?: return
-                avg += opinion.rating
-                count += 1
-                avg /= count
-                avg = (avg * 100.0f).roundToInt().toFloat() / 100f
-                ratingReference.setValue(avg)
-            }
-
-            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
-                var avg  = 0f
-                var count  = 0
-                var opinion = dataSnapshot.getValue(Opinion::class.java) ?: return
-                avg += opinion.rating
-                count += 1
-                avg /= count
-                avg = (avg * 100.0f).roundToInt().toFloat() / 100f
-                ratingReference.setValue(avg)
-            }
-
-            override fun onChildRemoved(snapshot: DataSnapshot) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })*/
     }
 }
